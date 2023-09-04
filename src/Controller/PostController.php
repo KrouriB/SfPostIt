517a1cf0ec2b611dd9db5ccb69a5a0a6743be080
KrouriB <brice.krouri@gmail.com>
2023-09-04 11:45:46 +0200

    envoie info a la vue (form plus les post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,19 +2,44 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
     #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    public function index(Request $request, PostRepository $postRepository, EntityManagerInterface $entityManager): Response
     {
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post');
+        }
+
+        $today = date('Y-m-d H:i:s');
+        $aFaire = $postRepository->findByPostAFaire();
+        $terminer = $postRepository->findByPostTerminer();
         return $this->render(
             'post/index.html.twig',
             [
-            'controller_name' => 'PostController',
+                'post' => $form->createView(),
+                'today' => $today,
+                'aFaire' => $aFaire,
+                'terminer' => $terminer,
             ]
         );
     }
