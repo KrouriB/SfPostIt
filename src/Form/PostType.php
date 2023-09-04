78d218d7b652c3d83e777fa88ac21c6516934d0e
KrouriB <brice.krouri@gmail.com>
2023-09-04 10:11:25 +0200

    Form

---
+++ src/Form/PostType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('dateLimite')
+            ->add('dateCreation')
+            ->add('information')
+            ->add('etat');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(
+            [
+            'data_class' => Post::class,
+            ]
+        );
+    }
+}
