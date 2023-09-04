317b8507a5e8cb1660906886a4873a0c8e891bc7
KrouriB <brice.krouri@gmail.com>
2023-09-04 15:19:26 +0200

    formulaire d'un post

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,19 +4,60 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Eckinox\TinymceBundle\Form\Type\TinymceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $dt = new \DateTime();
         $builder
-            ->add('titre')
-            ->add('dateLimite')
-            ->add('dateCreation')
-            ->add('information')
-            ->add('etat');
+            ->add(
+                'titre',
+                TextType::class,
+                [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'dateLimite',
+                DateTimeType::class,
+                [
+                    // 'widget' => 'single_text',
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'dateCreation',
+                HiddenType::class,
+                [
+                    // 'format' => 'YYYY-mm-dd HH:ii:ss',
+                    'data' => $dt->format('Y-m-d H:i:s'),
+                ]
+            )
+            ->add(
+                'information',
+                TinymceType::class,
+                [
+                    'attr' => [
+                        "toolbar" => "bold italic underline | bullist numlist",
+                    ]
+                ]
+            )
+            ->add(
+                'etat',
+                HiddenType::class, [
+                    'data'  => 'A faire',
+                ]);
         ;
     }
 
