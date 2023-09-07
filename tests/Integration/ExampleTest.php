82df0c2baee0965c73f8129e7779ff58dadb7a44
KrouriB <brice.krouri@gmail.com>
2023-09-07 14:56:05 +0200

    test change of data

--- tests/Integration/ExampleTest.php
+++ tests/Integration/ExampleTest.php
@@ -1,9 +1,18 @@
 <?php
 
-use App\Entity\Post;
-use App\DataFixtures\PostFixtures;
 use App\Repository\PostRepository;
-use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+// use Doctrine\Common\DataFixtures\Loader;
+// use MyDataFixtures\UserDataLoader;
+// use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+// use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+
+// beforeEach(function() {
+//     $loader = new Loader();
+//     $loader->loadFromFile('/src/DataFixtures/postFixture.php');
+//     $executor = new ORMExecutor($entityManager, new ORMPurger());
+//     $executor->execute($loader->getFixtures());
+// });
 
 it('should have 10 item from the query where the post is done', function(){
     
@@ -13,6 +22,22 @@ it('should have 10 item from the query where the post is done', function(){
     expect($finished)->toHaveCount(10);
 });
 
-// it('should have 20 item after modifing try', function(PostRepository $postRepository){
-//     $todo = $postRepository->findByPostAFaire();
-// });
+it('should have 20 item after modifing try', function(){
+    
+    $postRepository = $this->container->get(PostRepository::class);
+    $entityManager = $this->container->get(EntityManagerInterface::class);
+
+    $todo = $postRepository->findByPostAFaire();
+    foreach($todo as $post){
+        $post->done();
+        // dd($post);
+        $entityManager->persist($post);
+        $entityManager->flush();
+    }
+
+    $postRepository = $this->container->get(PostRepository::class);
+
+    $finished = $postRepository->findByPostTerminer();
+    // dd($finished);
+    expect($finished)->toHaveCount(20);
+});
