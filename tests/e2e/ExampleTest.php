519232b3f0697ef03cf6d52f745601e0c121c4ff
KrouriB <brice.krouri@gmail.com>
2023-09-07 15:54:39 +0200

    test page

--- tests/e2e/ExampleTest.php
+++ tests/e2e/ExampleTest.php
@@ -1,14 +1,35 @@
 <?php
 
-use App\Entity\Post;
-use App\DataFixtures\PostFixtures;
 use App\Repository\PostRepository;
-use Doctrine\Persistence\ObjectManager;
 
-// it('should show a page', function () {
-//     $this->client->request('GET' , '/bonjour');
+
+
+it('should show a page', function () {
+    $crawler = $this->client->request('GET' , '/post');
+
+    $this->assertSame(200, $this->client->getResponse()->getStatusCode());
     
-//     //remplir des formulaires
+    $this->assertSelectorTextContains('h2', 'PostIt A faire :');
+});
+
+it('should create a post', function () {
+    $postRepository = $this->container->get(PostRepository::class);
+    $crawler = $this->client->request('GET' , '/post/new');
+    // $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    dd($crawler);
+
+    $form = $crawler->selectButton('valider')->form();
+    // dd($form);
+    $form['titre']->setValue('Pest test');
+    $form['information']->setValue('Test via pest');
+    $form['dateLimite']->setValue('2023/12/23');
+
+    $this->client->submit($form);
     
-//     expect($this->container->get(PostRepository::class)->findAll()->count())->toBe(11);
-// }
+    $this->assertTrue($client->getResponse()->isRedirect());
+
+    $all = $postRepository->findAll();
+    // dd($all);
+    
+    expect($all)->toHaveCount(21);
+});
